5d66caefcf5f3c1d2c9c3185bbfd38c9c1a4f2a3
a4442277 <a422789255@gmail.com>
2025-11-03 02:35:07 +0500

    Edit the backend server and use fireBase

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import ModeContextProvider from './Components/ContextModeStat.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+   <HashRouter>
     <ModeContextProvider>
     <App />
     </ModeContextProvider>
-    </BrowserRouter>
+</HashRouter>
   </StrictMode>,
 )
